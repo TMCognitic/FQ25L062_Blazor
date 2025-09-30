15a19fb7bc7a232bc843ae05f6b3d528e95cf2e7
Thierry Morre <thierry.morre@cognitic.be>
2025-09-30 14:48:46 +0200

    Explication LifeCycle & Communication entre composants  Parent <-> Enfant

---
+++ FQ25L062_Blazor/Models/Entities/Personne.cs
@@ -0,0 +1,19 @@
+﻿namespace FQ25L062_Blazor.Models.Entities
+{
+    public class Personne
+    {
+        public int Id { get; }
+        public string Nom { get; }
+        public string Prenom { get; }
+        public string Email { get; }
+        public string Tel { get; }
+        public Personne(int id, string nom, string prenom, string email, string tel)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+            Tel = tel;
+        }
+    }
+}
