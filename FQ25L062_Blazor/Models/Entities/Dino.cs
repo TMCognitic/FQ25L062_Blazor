5a96a454d33fd44de0ed0898d93a6bd055fd7457
Thierry Morre <thierry.morre@cognitic.be>
2025-10-01 14:59:01 +0200

    Ajout Consommation API

---
+++ FQ25L062_Blazor/Models/Entities/Dino.cs
@@ -0,0 +1,18 @@
+﻿namespace FQ25L062_Blazor.Models.Entities
+{
+    public class Dino
+    {
+        public Dino(int id, string espece, int poids, int taille)
+        {
+            Id = id;
+            Espece = espece;
+            Poids = poids;
+            Taille = taille;
+        }
+
+        public int Id { get; }
+        public string Espece { get; }
+        public int Poids { get; }
+        public int Taille { get; }
+    }
+}
--- FQ25L062_Blazor/Program.cs
+++ FQ25L062_Blazor/Program.cs
@@ -8,4 +8,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//Nécessite le package nuget Microsoft.Extensions.Http
+builder.Services.AddHttpClient(ApiName.DinoManagerApi, options =>
+{
+    options.BaseAddress = new Uri("https://localhost:7093/");
+});
 await builder.Build().RunAsync();
+
+static class ApiName
+{
+    public const string DinoManagerApi = "DinoManagerApi";
+}
